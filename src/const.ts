440bffb9dd84c4ad4922c5c3caccfeba5b221139
ngdlong91 <ngdlong91@gmail.com>
2023-12-11 10:07:59 +0700

    Update contract

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
 import {Address} from "@graphprotocol/graph-ts";
 
 
-export const ContractAddress = Address.fromString("0xD66B26c743fc57985559DE7D1A85220190B4bbBd")
+export const ContractAddress = Address.fromString("0x34AdaCBbb61AB0345094fFdFe2a6a737530BF155")
