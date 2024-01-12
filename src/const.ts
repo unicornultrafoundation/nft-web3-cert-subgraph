54499c171a11ad148af884f0af6eca448ab11172
ngdlong91 <ngdlong91@gmail.com>
2024-01-12 12:09:10 +0700

    Init

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
 import {Address} from "@graphprotocol/graph-ts";
 
 
-export const ContractAddress = Address.fromString("0xC102BfEeE0674798Df49618E12b45FcD8e7C0EC0")
+export const ContractAddress = Address.fromString("0x8D244EF3f66be3910cbb16Fc63b85C1d67B3F17F")
